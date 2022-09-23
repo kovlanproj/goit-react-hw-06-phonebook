6e824cacffa3b935ccdbd9ea57c8d27aa2f6656b
kovlanproj <kozorizva@gmail.com>
2022-09-23 23:01:52 +0300

    persist

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,19 +1,19 @@
 import { configureStore } from '@reduxjs/toolkit';
 import contactsReducer from './contactsSlice';
-// import { persistStore, persistReducer } from 'redux-persist';
-// import storage from 'redux-persist/lib/storage';
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
 
-// const persistConfig = {
-//   key: 'root',
-//   storage,
-// };
+const persistConfig = {
+  key: 'root',
+  storage,
+};
 
-// const persistedReducer = persistReducer(persistConfig, contactsReducer);
+const persistedReducer = persistReducer(persistConfig, contactsReducer);
 
 export const store = configureStore({
   reducer: {
-    contacts: contactsReducer,
+    contacts: persistedReducer,
   },
 });
 
-// export let persistor = persistStore(store);
+export const persistor = persistStore(store);
